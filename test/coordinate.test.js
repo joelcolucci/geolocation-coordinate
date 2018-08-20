b93d295860e2dbb9e4a4ff7bcb2c125d65c3dca3
joelcolucci <joelcolucci@gmail.com>
2018-08-20 08:59:54 -0400

    test: add tests for all cardinal directions

--- test/coordinate.test.js
+++ test/coordinate.test.js
@@ -34,33 +34,63 @@ describe('Coordinate properties', () => {
 
 
 describe('Coordinate toString instance method', () => {
-  let coord;
+  let northernLatitude;
+  let southernLatitude;
+  let easternLongitude;
+  let westernLongitude;
 
   beforeEach(() => {
-    coord = new Coordinate(59.2349887712, 'latitude');
+    northernLatitude = new Coordinate(59.2349887712, 'latitude');
+    southernLatitude = new Coordinate(-59.2349887712, 'latitude');
+    easternLongitude = new Coordinate(59.2349887712, 'longitude');
+    westernLongitude = new Coordinate(-59.2349887712, 'longitude');
   });
 
   test('toString - default - dd', () => {
-    let coordString = coord.toString();
+    let northernLatitudeString = northernLatitude.toString();
+    let southernLatitudeString = southernLatitude.toString();
+    let easternLongitudeString = easternLongitude.toString();
+    let westernLongitudeString = westernLongitude.toString();
 
-    expect(coordString).toEqual('59.235° North');
+    expect(northernLatitudeString).toEqual('59.235° North');
+    expect(southernLatitudeString).toEqual('59.235° South');
+    expect(easternLongitudeString).toEqual('59.235° East');
+    expect(westernLongitudeString).toEqual('59.235° West');
   });
 
   test('toString - decimal degrees - dd', () => {
-    let coordString = coord.toString('dd');
+    let northernLatitudeString = northernLatitude.toString('dd');
+    let southernLatitudeString = southernLatitude.toString('dd');
+    let easternLongitudeString = easternLongitude.toString('dd');
+    let westernLongitudeString = westernLongitude.toString('dd');
 
-    expect(coordString).toEqual('59.235° North');
+    expect(northernLatitudeString).toEqual('59.235° North');
+    expect(southernLatitudeString).toEqual('59.235° South');
+    expect(easternLongitudeString).toEqual('59.235° East');
+    expect(westernLongitudeString).toEqual('59.235° West');
   });
 
   test('toString - degrees, decimal minutes - ddm', () => {
-    let coordString = coord.toString('ddm');
+    let northernLatitudeString = northernLatitude.toString('ddm');
+    let southernLatitudeString = southernLatitude.toString('ddm');
+    let easternLongitudeString = easternLongitude.toString('ddm');
+    let westernLongitudeString = westernLongitude.toString('ddm');
 
-    expect(coordString).toEqual(`59° 14.099' North`);
+    expect(northernLatitudeString).toEqual(`59° 14.099' North`);
+    expect(southernLatitudeString).toEqual(`59° 14.099' South`);
+    expect(easternLongitudeString).toEqual(`59° 14.099' East`);
+    expect(westernLongitudeString).toEqual(`59° 14.099' West`);
   });
 
   test('toString - degrees, minutes, seconds - dms', () => {
-    let coordString = coord.toString('dms');
+    let northernLatitudeString = northernLatitude.toString('dms');
+    let southernLatitudeString = southernLatitude.toString('dms');
+    let easternLongitudeString = easternLongitude.toString('dms');
+    let westernLongitudeString = westernLongitude.toString('dms');
 
-    expect(coordString).toEqual(`59° 14' 6" North`);
+    expect(northernLatitudeString).toEqual(`59° 14' 6" North`);
+    expect(southernLatitudeString).toEqual(`59° 14' 6" South`);
+    expect(easternLongitudeString).toEqual(`59° 14' 6" East`);
+    expect(westernLongitudeString).toEqual(`59° 14' 6" West`);
   });
 });
